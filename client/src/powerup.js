c18bfeddd91765a856f09c06591a9e36475edad9
samruddhi-19 <samruddhibarahate@gmail.com>
2026-05-21 13:39:01 +0530

    added icon to board buttons

--- client/src/powerup.js
+++ client/src/powerup.js
@@ -4,11 +4,17 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function (t, options) {
     return [{
       text: 'Cardlytics',
+      
+      icon: {
+        dark: 'https://cardlytics-powerup.vercel.app/logo.png',
+        light: 'https://cardlytics-powerup.vercel.app/logo.png'
+      },
+
       callback: function (t) {
         return t.modal({
           title: 'Cardlytics',
           url: './index.html',
-          fullscreen: false   // 👈 keeps it centered modal
+          fullscreen: false
         });
       }
     }];
