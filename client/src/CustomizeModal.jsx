7e7dbca8d8eb23efee7e1bd46cd24921b912b992
samruddhi-19 <samruddhibarahate@gmail.com>
2026-06-19 14:25:29 +0530

    changed maxheight and maxwidth

--- client/src/CustomizeModal.jsx
+++ client/src/CustomizeModal.jsx
@@ -916,7 +916,7 @@ function CardConfigModal({
         border: `1px solid ${T.border}`,
         borderRadius: 14,
         width: "calc(100vw - 16px)",
-        maxWidth: "calc(100vw - 16px)",
+        maxWidth: 1120,
         maxHeight: "calc(100vh - 16px)",
         display: "flex",
         flexDirection: "column",
