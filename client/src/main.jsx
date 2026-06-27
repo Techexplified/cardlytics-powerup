4faf0f08a944024ddc02d4a0e4fe1e8677226d2e
samruddhi-19 <samruddhibarahate@gmail.com>
2026-06-27 19:06:08 +0530

    fixed1

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import CheckoutPage from "./pages/CheckoutPage";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+const params = new URLSearchParams(window.location.search);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {params.get("_ptxn")
+      ? <CheckoutPage />
+      : <App />}
+  </React.StrictMode>
+);
